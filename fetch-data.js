9a732c840069740c0a03003672e61b689f41dc6c
Kiptum <duncankipkoech12@gmail.com>
2025-08-02 20:44:42 +0300

    response

---
+++ fetch-data.js
@@ -0,0 +1,28 @@
+async function fetchUserData(){
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById('api-data' );
+
+    try{
+        const response = await fetch(apiUrl);
+        const users = await response.json(); 
+        dataContainer.innerHTML = '';
+        const userList = document.createElement('ul');
+
+        users.forEach(user => {
+           let list =  document.createElement('li');
+           list.textContent = user.name;
+           userList.appendChild(list);            
+        });
+
+        dataContainer.appendChild(userList);
+    }
+
+    catch{
+        dataContainer.innerHTML = '';
+        dataContainer.textContent = 'Failed to load user data.'; 
+
+    }
+
+}
+
+fetchUserData()
